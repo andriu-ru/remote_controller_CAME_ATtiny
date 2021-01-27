40f540d82ead1f16cd6de854e1c985fe4917157c
Andriu-ru <svjazinet@yandex.ru>
2021-01-27 23:39:37 +0300

    Изменил алгоритм отправки ключа в эфир. Изменил последовательность записи ключа в массив при переводе его в двоичную систему исчисления.

--- remote_controller_CAME_ATtiny/Debug/main.d
+++ remote_controller_CAME_ATtiny/Debug/main.d
@@ -4,7 +4,7 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.8.332\include/avr/iotn13a.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.8.332\include/avr/iotn25.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
@@ -12,7 +12,8 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -24,7 +25,7 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.8.332\include/avr/iotn13a.h:
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATtiny_DFP\1.8.332\include/avr/iotn25.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
@@ -41,3 +42,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
--- remote_controller_CAME_ATtiny/main.c
+++ remote_controller_CAME_ATtiny/main.c
@@ -8,33 +8,43 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 #define start_delay 11520 //Начальная задержка по стандарту протокола 11520мкс
 #define one_stop 320 //по стандарту протокола задержка должна быть 320мкс
 #define two_stop 640 //по стандарту протокола задержка должна быть 640мкс
+
 
 int key = 546; //Код пульта
 
 int maska = 1; //Данная маска поможет разбить key на биты и поместить его в массив massiv
 char massiv [12] = {0,0,0,0,0,0,0,0,0,0,0,0};
 int a = 0; //дополнительная переменная, содержит количество байт ключа при передаче в эфир
+char z = 0;
 	
 	void stop_comparator (void); //Функция отключает аналоговый компаратор
+	//void TC0_setup (void); //Функция настройки таймера счетчика
 	int razbivka (int z); //Функция считывает при помощи маски maska первый бит переменной z 
 	void setup_ports (void);  //Функция начальной настройки портов процессора
 	//void setup_mk (void); //Функция настроки параметров микроконтроллера
 	void one (void); //Вызывая данную функцию получаем 1 в интерфейсе
 	void zero (void); //Вызывая данную функцию получаем 0 в интерфейсе
 	//void test_frequency (void); //Проверка частоты процессора 
+	void key_out (void); //Функция выдает ключь в эфир
+	
+// 	ISR (TIM0_COMPA_vect)
+// 	{
+// 		z = 1;
+// 	}
 
 int main(void) //Начало функции main
 {
-    
+    //TC0_setup();
 	setup_ports(); //Насройка портов процессора
 	
 	stop_comparator(); //Отключаем аналоговый компаратор
 	
-	for (int i = 0; i < 12; ++i) //в цикле разбиваем число содержащееся в переменной key на отдельные биты и помещаем его в массив massiv
+	for (int i = 11; i >= 0; --i) //в цикле разбиваем число содержащееся в переменной key на отдельные биты и помещаем его в массив massiv
 	{
 		massiv[i] = razbivka(key);
 		key = key/2;
@@ -50,42 +60,50 @@ int main(void) //Начало функции main
 			PORTB |= (1<<PB2); //Светодиод индицирующий работу пульта
 			
 			PORTB |= (1<<PB1);
-			_delay_ms(100);
+			_delay_ms(5);
 			PORTB &= ~(1<<PB1);
 			_delay_us(start_delay); //Пилотный период
 			
-			while(~PINB & (1<<PB0)) //Стартовый цикл while. Если кнопка пульта нажата начинаем генерить ключь в эфир
-			{
+// 			while(~PINB & (1<<PB0)) //Стартовый цикл while. Если кнопка пульта нажата начинаем генерить ключь в эфир
+// 			{
+	           
 				
 				PORTB |= (1<<PB1); //Стартовый бит
 				_delay_us(one_stop);
+				PORTB &= ~(1<<PB1);
 				
+				key_out();
+				
+				/*
 					while(~PINB & (1<<PB0)) //Кодовый цикл while. Если кнопка пульта нажата выдаем в эфир 12 битный ключь
 					{
-						if (0 == (massiv[a] & (1<<0)))
+						while (a<=12)
 						{
-							zero();
+							if (0 == (massiv[a] & (1<<0)))
+							{
+								zero();
+							}
+							
+							else
+							{
+								one();
+							}
+							
+							a++;
 						}
-						
-						else
-						{
-							one();
-						}
-						
-						a++;
 						
 						if (a>=12) break;
 						
 					} //Конец кодового цикла while
-				
-					if (a>=12) break;		
-				
-			} //Конец стартового цикла while  
+	
+					//if (a>=12) break;
+	
+			//} //Конец стартового цикла while  
 			
 			a = 0;
-			
+			*/
 			PORTB &= ~(1<<PB1);
-			_delay_ms(5);
+			_delay_ms(2);
 			
 		} //Конец проверки нажатия клавиши
 		
@@ -123,15 +141,21 @@ void zero (void) //Вызывая данную функцию получаем 0 в интерфейсе
 	PORTB &= ~(1<<PB1); //Первый бит "0"
 	_delay_us(one_stop);
 	PORTB |= (1<<PB1);
-	_delay_us(two_stop);
+	_delay_us(one_stop);
+	_delay_us(one_stop);
+	//_delay_us(two_stop);
+	
 }
 
 void one (void) //Вызывая данную функцию получаем 1 в интерфейсе
 {
 	PORTB &= ~(1<<PB1); //Третий бит "1"
-	_delay_us(two_stop);
+	//_delay_us(two_stop);
+	_delay_us(one_stop);
+	_delay_us(one_stop);
 	PORTB |= (1<<PB1);
 	_delay_us(one_stop);
+	
 }
 
 // void test_frequency (void) //Проверка частоты процессора
@@ -139,3 +163,31 @@ void one (void) //Вызывая данную функцию получаем 1 в интерфейсе
 // 	PORTB ^= (1<<PB2);
 // 	_delay_ms(1000);
 // }
+
+// void TC0_setup (void) //Функция настройки таймера счетчика
+// {
+// 	TCCR0A |= (1<<WGM01);
+// 	TCCR0B &= ~((1<<CS02)|(1<<CS00));
+// 	TCCR0B |= (1<<CS01);
+// 	OCR0A = 0xFF;
+// 	
+// 	GTCCR |= (1<<TSM)|(1<<PSR0);
+// 	TCNT0 = 0;
+// 	GTCCR &= ~((1<<TSM) | (1<<PSR0));
+// }
+
+void key_out (void)
+{
+	massiv[0] == 0 ? zero() : one();
+	massiv[1] == 0 ? zero() : one();
+	massiv[2] == 0 ? zero() : one();
+	massiv[3] == 0 ? zero() : one();
+	massiv[4] == 0 ? zero() : one();
+	massiv[5] == 0 ? zero() : one();
+	massiv[6] == 0 ? zero() : one();
+	massiv[7] == 0 ? zero() : one();
+	massiv[8] == 0 ? zero() : one();
+	massiv[9] == 0 ? zero() : one();
+	massiv[10] == 0 ? zero() : one();
+	massiv[11] == 0 ? zero() : one();
+}
